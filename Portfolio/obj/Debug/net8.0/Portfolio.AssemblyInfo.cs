132dede95f9ed059e6bdc0e7dfa45cda01defa2c
Taylor McLaughlin <158223645+kalewashere@users.noreply.github.com>
2025-02-26 14:59:48 +0000

    Portfolio - Now with comments!

--- Portfolio/obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9660276b1f6742a90aa8b9137725bdac02588dd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b47071c6582a7ea9f58095174089469ab67f021a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
